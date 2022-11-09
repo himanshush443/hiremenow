8ac7ee480dbe6dd9f48e16e58ef238091b5189cc
Himanshu Khare <himanshush443@gmail.com>
2022-11-09 16:56:19 +0530

    routes and controller and error handling started

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,11 @@
+import { StatusCodes } from 'http-status-codes';
+
 const errorHandleMiddleware = (err, req, res, next) => {
-  res.status(500).json({ msg: 'there was an error' });
+  const defaultError = {
+    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: 'Something went wrong, try again later',
+  };
+  res.status(defaultError.statusCode).json({ msg: err });
 };
 
 export default errorHandleMiddleware;
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import express from 'express';
 const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
+import 'express-async-errors';
 
 // db and authenticateUser
 import connectDB from './db/connect.js';
 
 // routers
 import authRouter from './routes/authRoutes.js';
+import jobsRouter from './routes/jobsRoutes.js';
 
 // middleware
 import notFoundMiddleware from './middleware/not-found.js';
@@ -20,10 +22,11 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandleMiddleware);
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
